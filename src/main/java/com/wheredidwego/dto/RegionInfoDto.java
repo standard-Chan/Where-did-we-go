814c99db80b7dbdd46e0cb7f8264368726bee973
Jeong SeokChan <starp321@naver.com>
2025-04-13 15:10:56 +0900

    feat: GeocodingException 추가

---
+++ src/main/java/com/wheredidwego/dto/RegionInfoDto.java
@@ -0,0 +1,4 @@
+package com.wheredidwego.dto;
+
+public class RegionInfoDto {
+}
---
+++ src/main/java/com/wheredidwego/exception/GeocodingException.java
@@ -0,0 +1,7 @@
+package com.wheredidwego.exception;
+
+public class GeocodingException extends RuntimeException {
+    public GeocodingException(String message) {
+        super(message);
+    }
+}
