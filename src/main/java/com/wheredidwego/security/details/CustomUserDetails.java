2c83fa185e38d7e5f0d3de607ef797f2204fd0ff
Jeong SeokChan <starp321@naver.com>
2025-04-15 12:05:20 +0900

    feat : CustomUserDetails 인증객체 구현

--- src/main/java/com/wheredidwego/security/details/CustomUserDetails.java
+++ src/main/java/com/wheredidwego/security/details/CustomUserDetails.java
@@ -1,4 +1,36 @@
 package com.wheredidwego.security.details;
 
-public class CustomUserDetails {
+import com.wheredidwego.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public String getNickname() {
+        return user.getNickname();
+    }
 }
--- src/main/java/com/wheredidwego/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/wheredidwego/security/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,15 @@
 package com.wheredidwego.security.filter;
 
+import com.wheredidwego.domain.User;
+import com.wheredidwego.repository.UserRepository;
+import com.wheredidwego.security.details.CustomUserDetails;
 import com.wheredidwego.util.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,9 +19,11 @@ import java.util.List;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
-    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserRepository userRepository) {
         this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -39,10 +45,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         String email = jwtUtil.extractEmail(token);
+        User user = userRepository.findUserByEmail(email).orElseThrow(() -> new RuntimeException("존재하지 않는 회원입니다.") {
+        });
+        CustomUserDetails userDetails = new CustomUserDetails(user);
 
         // 인증 객체 생성
         UsernamePasswordAuthenticationToken authentication =
-                new UsernamePasswordAuthenticationToken(email, null, List.of());
+                new UsernamePasswordAuthenticationToken(userDetails, null, List.of());
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
