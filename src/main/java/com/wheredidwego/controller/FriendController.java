cba3932abced7d239c7cf177e070ea243923f4ca
standard-chan <starp321@naver.com>
2025-05-13 23:33:12 +0900

    feat : Friend Controller Exception 추가

--- src/main/java/com/wheredidwego/controller/FriendController.java
+++ src/main/java/com/wheredidwego/controller/FriendController.java
@@ -4,6 +4,7 @@ import com.wheredidwego.domain.Friend;
 import com.wheredidwego.domain.User;
 import com.wheredidwego.dto.friendDto.FriendResponseDto;
 import com.wheredidwego.dto.friendDto.FriendUpdateDto;
+import com.wheredidwego.exception.FriendException;
 import com.wheredidwego.security.details.CustomUserDetails;
 import com.wheredidwego.service.FriendService;
 import com.wheredidwego.service.UserService;
@@ -25,30 +26,43 @@ public class FriendController {
     @GetMapping()
     public ResponseEntity<?> getFriends(@AuthenticationPrincipal CustomUserDetails userDetails) {
 
-        User user = userService.findUserByEmail(userDetails.getUsername());
-        Set<Friend> friends = friendService.getFriendsSetByUser(user);
-        List<FriendResponseDto> response = friends.stream().map(FriendResponseDto::new).toList();
-        return ResponseEntity.ok().body(response);
+        try {
+            User user = userService.findUserByEmail(userDetails.getUsername());
+            Set<Friend> friends = friendService.getFriendsSetByUser(user);
+            List<FriendResponseDto> response = friends.stream().map(FriendResponseDto::new).toList();
+            return ResponseEntity.ok().body(response);
+        } catch (FriendException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 
     @PutMapping()
     public ResponseEntity<?> updateFriendInfo(@AuthenticationPrincipal CustomUserDetails userDetails,
                                               @RequestBody FriendUpdateDto friendUpdateDto) {
-        Friend friend = friendService.getFriendById(friendUpdateDto.getFriendEntityId());
-        Friend updatedFriend = friendService.updateFriend(friend, friendUpdateDto.getAccessLevel(), friendUpdateDto.getDescription());
-        FriendUpdateDto responseDto = new FriendUpdateDto(updatedFriend);
-        return ResponseEntity.ok(responseDto);
+        try {
+            Friend friend = friendService.getFriendById(friendUpdateDto.getFriendEntityId());
+            Friend updatedFriend = friendService.updateFriend(friend, friendUpdateDto.getAccessLevel(), friendUpdateDto.getDescription());
+            FriendUpdateDto responseDto = new FriendUpdateDto(updatedFriend);
+            return ResponseEntity.ok(responseDto);
+        } catch (FriendException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @DeleteMapping("/{friendEmail}")
-    public ResponseEntity<?> deleteFriend(@PathVariable("friendEmail")String friendEmail,
+    public ResponseEntity<?> deleteFriend(@PathVariable("friendEmail") String friendEmail,
                                           @AuthenticationPrincipal CustomUserDetails userDetails) {
-        User user = userService.findUserByEmail(userDetails.getUsername());
-        User friendUser = userService.findUserByEmail(friendEmail);
+        try {
+            User user = userService.findUserByEmail(userDetails.getUsername());
+            User friendUser = userService.findUserByEmail(friendEmail);
 
-        friendService.deleteFriend(user, friendUser);
+            friendService.deleteFriend(user, friendUser);
 
-        return ResponseEntity.ok().build();
+            return ResponseEntity.ok().build();
+        } catch (FriendException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
--- src/main/java/com/wheredidwego/service/FriendRequestService.java
+++ src/main/java/com/wheredidwego/service/FriendRequestService.java
@@ -49,9 +49,8 @@ public class FriendRequestService {
             throw new FriendRequestException("이미 친구상태인 유저입니다.");
         }
 
-        // 해당 친구 요청을 이미 보낸 경우 (승인/보류의 경우에 한해서)
-        if (friendRequestRepository.existsFriendRequestBySenderAndReceiverAndStatus(sender, receiver, RequestStatus.ACCEPTED)
-        || friendRequestRepository.existsFriendRequestBySenderAndReceiverAndStatus(sender, receiver, RequestStatus.PENDING)) {
+        // 해당 친구 요청을 이미 보낸 경우 (보류된 경우)
+        if (friendRequestRepository.existsFriendRequestBySenderAndReceiverAndStatus(sender, receiver, RequestStatus.PENDING)) {
             throw new FriendRequestException("친구 요청을 이미 보냈습니다.");
         }
 
